d173d9e76f5caca80895cca5cb2a5755ee81c95e
Chris Campbell <ccampbell@zynga.com>
2012-02-22 21:57:43 -0800

    initial commit

---
+++ atom-test.js
@@ -0,0 +1,266 @@
+/*global atom, clock, console, require, test*/
+atom = typeof atom == 'undefined' ? require('./atom') : atom;
+clock = typeof clock == 'undefined' ? require('./clock') : clock;
+test = typeof test == 'undefined' ? require('./test') : test;
+
+var
+	a = atom.create(),
+	session = test.session(),
+	assert = session.assert,
+	results = []
+;
+
+a.set('a', 'A');
+assert('get() returns a single value', a.get('a') == 'A');
+
+a.set('b', 'B');
+assert('get() returns a list of values', a.get(['a', 'b']) + '' == 'A,B');
+
+a.get('a', function (a) {
+	results.push(a);
+});
+assert('get() calls back with a single value', results + '' == 'A');
+
+results = [];
+a.get(['a', 'b'], function (a, b) {
+	results = results.concat([a, b]);
+});
+assert('get() calls back with a list of values', results == 'A,B');
+
+a.set('c', 'C');
+assert('set() sets a single value', a.get('c') == 'C');
+
+a.set({ d: 'D', e: 'E' });
+assert('set() sets a map of values', a.get('d') == 'D' && a.get('e') == 'E');
+
+assert('has() works', a.has('a') && !a.has('f'));
+
+assert('keys() works', a.keys() + '' == 'a,b,c,d,e');
+
+results = [];
+function aListener(a) {
+	results.push(a);
+}
+a.bind('a', aListener);
+a.set('a', 'A1');
+assert('bind() works when called with a single key', results + '' == 'A1');
+
+a.bind(['b', 'c'], function (b, c) {
+	results = results.concat([b, c]);
+});
+a.set('b', 'B1');
+assert('bind() works when called with a list', results + '' == 'A1,B1,C');
+
+a.set('a', 'A2');
+a.unbind(aListener);
+a.set('a', 'A3');
+assert('unbind() prevents the function from being called again',
+	results + '' == 'A1,B1,C,A2');
+
+results = [];
+a.once('c', function (c) {
+	results.push(c);
+});
+assert('once() works when called with one condition that is already complete',
+	results + '' == 'C');
+
+results = [];
+a.once(['b', 'a'], function (b, a) {
+	results = results.concat([b, a]);
+});
+assert('once() works when called with two conditions that are already ' +
+	'complete', results + '' == 'B1,A3');
+
+results = ['set'];
+a.set('done');
+a.once('done', function (val) {
+	results = results.concat(['once', val]);
+});
+assert('once() works when the conditions are set with no value provided',
+	results + '' == 'set,once,');
+
+results = [];
+a.once('f', function (f) {
+	results.push('once');
+	results.push(f);
+});
+results.push('set');
+a.set('f', 'F');
+assert('once() works when called with one condition that is not complete',
+	results + '' == 'set,once,F');
+
+results = [];
+a.once(['g', 'h'], function (g, h) {
+	results = results.concat(['once', g, h]);
+});
+results.push('set');
+a.set({ g: 'G', h: 'H' });
+assert('once() works when called with multiple conditions that are ' +
+	'not complete', results + '' == 'set,once,G,H');
+
+results = [];
+a.once(['h', 'i'], function (h, i) {
+	results = results.concat(['once', h, i]);
+});
+results.push('set');
+a.set('i', 'I');
+assert('once() works when called with a mix of complete and incomplete ' +
+	'conditions', results + '' == 'set,once,H,I');
+
+var aCalls = 0;
+a.once('a', function () {
+	aCalls++;
+});
+a.set('a', 'A3');
+assert('once() gets called only once, even when conditions are completed ' +
+	'multiple times', aCalls == 1);
+
+results = [];
+a.next('a', function (a) {
+	results.push(a);
+});
+a.set('a', 'A4');
+assert('next() works when called with a single condition',
+	results + '' == 'A4');
+
+results = [];
+a.next(['b', 'c'], function (b, c) {
+	results = results.concat(['next', b, c]);
+});
+results.push('set');
+a.set('c', 'C1');
+assert('next() works when called with multiple conditions',
+	results + '' == 'set,B1,C1,next,B1,C1');
+
+results = [];
+a.next('x', function (y) {
+	results.push('x=' + y);
+	a.next('x1', function (y1) {
+		results.push('x1=' + y1);
+	});
+});
+results.push('setX');
+a.set('x', 'y');
+results.push('setX1');
+a.set('x1', 'y1');
+assert('set() takes it in stride when the listener list is synchronously ' +
+	'modified by one of the listeners', results + '' == 'setX,x=y,setX1,x1=y1');
+
+results = ['need'];
+a.need('d', function (d) {
+	results = results.concat(['satisfy', d]);
+});
+assert('need() calls back immediately when the (single) need is pre-satisfied',
+	results + '' == 'need,satisfy,D');
+
+results = ['need'];
+a.need(['e', 'f'], function (e, f) {
+	results = results.concat(['satisfy', e, f]);
+});
+assert('need() calls back immediately when the needs (plural) are pre-satisfied',
+	results + '' == 'need,satisfy,E,F');
+
+results = ['need'];
+a.need('j', function (j) {
+	results = results.concat(['satisfy', j]);
+});
+results.push('set');
+a.set('j', 'J');
+a.set('j', 'J1');
+assert('need() callback gets triggered after the needed value is set()',
+	results + '' == 'need,set,satisfy,J');
+
+results = ['need'];
+a.need('k', function (k) {
+	results = results.concat(['satisfy', k]);
+});
+results.push('provide');
+a.provide('k', function (done) {
+	results.push('fulfill');
+	done('K');
+});
+assert('need() registered before provide() works',
+	results + '' == 'need,provide,fulfill,satisfy,K');
+
+results = ['provide'];
+a.provide('l', function (done) {
+	results.push('fulfill');
+	done('L');
+});
+results.push('need');
+a.need('l', function (l) {
+	results = results.concat(['satisfy', l]);
+});
+assert('need() registered after provide() works',
+	results + '' == 'provide,need,fulfill,satisfy,L');
+
+results = [];
+a.chain(function (nextLink) {
+	results.push(1);
+	nextLink(2);
+});
+a.chain(function (nextLink, lastArg) {
+	results.push(lastArg);
+	results.push(3);
+	nextLink(4, 5);
+});
+a.chain(
+	function (nextLink, lastArg1, lastArg2) {
+		results.push(lastArg1);
+		results.push(lastArg2);
+		results.push(6);
+		nextLink(7);
+	},
+	function (nextLink, lastArg) {
+		results.push(lastArg);
+		results.push(8);
+		nextLink();
+	}
+);
+assert('chain() works', results + '' == '1,2,3,4,5,6,7,8');
+
+results = [];
+a.each(['a', 'b', 'd', 'c'], function (name, val) {
+	results.push(name + '=' + val);
+});
+assert('each() works', results + '' == 'a=A4,b=B1,d=D,c=C1');
+
+results = [];
+var otherAtom = atom.create();
+a.entangle(otherAtom, 'e');
+a.next('e', function (e) {
+	results = results.concat(['next', e]);
+});
+results.push('set');
+otherAtom.set('e', 'E1');
+assert('entangle() works for a single key', results + '' == 'set,next,E1');
+
+results = [];
+a.entangle(otherAtom, ['f', 'g']);
+otherAtom.once(['f', 'g'], function (f, g) {
+	results = results.concat(['once', f, g]);
+});
+results.push('set');
+a.set({ f: 'F1', g: 'G1' });
+assert('entangle() works for a list of keys', results + '' == 'set,once,F1,G1');
+
+results = [];
+a.entangle(otherAtom, {
+	m: 'oM',
+	n: 'oN'
+});
+otherAtom.once(['oM', 'oN'], function (oM, oN) {
+	results = results.concat(['once', oM, oN]);
+});
+results.push('set');
+a.set({ m: 'M1', n: 'N1' });
+assert('entangle() works for maps', results + '' == 'set,once,M1,N1');
+
+
+session.chain(function () {
+	var brief = session.brief();
+	if (brief.length) {
+		console.log(brief);
+	}
+	console.log(session.tally());
+});
---
+++ atom.js
@@ -0,0 +1,321 @@
+/*global module*/
+(function () {
+
+	// Establish the root object
+	var
+		root = this, // 'window' or 'global'
+		atom = { VERSION: '0.0.4' },
+		previous = root.atom
+	;
+	if (typeof module !== 'undefined' && module.exports) {
+		module.exports = atom;
+	}
+	root.atom = atom;
+
+	atom.noConflict = function () {
+		root.atom = previous;
+		return this;
+	};
+
+	// Convenience methods
+	var slice = Array.prototype.slice;
+	function isObject(obj) {
+		return typeof obj == 'object';
+	}
+	var isArray = Array.isArray || function (obj) {
+		return Object.prototype.toString.call(obj) === '[object Array]';
+	};
+	function inArray(arr, value) {
+		for (var i = arr.length; --i >= 0;) {
+			if (arr[i] === value) {
+				return true;
+			}
+		}
+	}
+	function toArray(obj) {
+		return isArray(obj) ? obj : [obj];
+	}
+
+	// Property getter
+	function get(nucleus, keyOrList, func, requireAll) {
+		var isList = isArray(keyOrList), keys = isList ? keyOrList : [keyOrList],
+			key, values = [], props = nucleus.props;
+		for (var i = keys.length; --i >= 0;) {
+			key = keys[i];
+			if (requireAll && !props.hasOwnProperty(key)) {
+				return;
+			}
+			values.unshift(props[key]);
+		}
+		return func ? func.apply({}, values) : isList ? values : values[0];
+	}
+
+	function removeListener(listeners, listener) {
+		for (var i = listeners.length; --i >= 0;) {
+			if (!listeners[i].calls) {
+				return listeners.splice(i, 1);
+			}
+		}
+	}
+
+	// Property setter
+	function set(nucleus, key, value) {
+		var keys, listener, listeners = nucleus.listeners,
+			listenersCopy = [].concat(listeners), requireAll, values,
+			props = nucleus.props, oldValue = props[key],
+			had = props.hasOwnProperty(key);
+		props[key] = value;
+		if (!had || oldValue !== value || (value && typeof value == 'object')) {
+			for (var i = listenersCopy.length; --i >= 0;) {
+				listener = listenersCopy[i];
+				keys = listener.keys;
+				requireAll = listener.all;
+				if (inArray(keys, key)) {
+					values = get(nucleus, keys, null, requireAll);
+					if (values || !requireAll) {
+						listener.cb.apply({}, values);
+						listener.calls--;
+					}
+					if (!listener.calls) {
+						removeListener(listeners, listener);
+					}
+				}
+			}
+			delete nucleus.needs[key];
+		}
+	}
+
+	function provide(nucleus, key, provider) {
+		provider(function (result) {
+			set(nucleus, key, result);
+		});
+	}
+
+	atom.create = function () {
+		var
+			nucleus = { props: {}, needs: {}, providers: {}, listeners: [] },
+			props = nucleus.props,
+			needs = nucleus.needs,
+			providers = nucleus.providers,
+			listeners = nucleus.listeners,
+			q = []
+		;
+		function doNext() {
+			if (q) {
+				q.pending = q.next = (!q.next && q.length) ?
+					q.shift() : q.next;
+				q.args = slice.call(arguments, 0);
+				if (q.pending) {
+					q.next = null;
+					q.pending.apply({}, [doNext].concat(q.args));
+				}
+			}
+		}
+		var me = {
+
+			// Call `func` whenever any of the specified keys change.  The values
+			// of the keys will be provided as args to func.
+			bind: function (keyOrList, func) {
+				listeners.unshift({ keys: toArray(keyOrList), cb: func,
+					all: false, calls: Infinity });
+			},
+
+			// Add a function or functions to the async queue.  Functions added
+			// thusly must call their first arg as a callback when done.  Any args
+			// provided to the callback will be passed in to the next function in
+			// the queue.
+			chain: function () {
+				if (q) {
+					for (var i = 0, len = arguments.length; i < len; i++) {
+						q.push(arguments[i]);
+						if (!q.pending) {
+							doNext.apply({}, q.args);
+						}
+					}
+				}
+			},
+
+			// Remove references to all properties and listeners.  This releases
+			// memory, and effective stops the atom from working.
+			destroy: function () {
+				delete nucleus.props;
+				delete nucleus.needs;
+				delete nucleus.providers;
+				delete nucleus.listeners;
+				while (q.length) {
+					q.pop();
+				}
+				nucleus = props = needs = providers = listeners =
+					q = q.pending = q.next = q.args = null;
+			},
+
+			// Call `func` on each of the specified keys.  The key is provided as
+			// the first arg, and the value as the second.
+			each: function (keyOrList, func) {
+				var keys = toArray(keyOrList), i = -1, len = keys.length, key;
+				while (++i < len) {
+					key = keys[i];
+					func(key, me.get(key));
+				}
+			},
+
+			// Establish two-way binding between a key or list of keys for two
+			// different atoms, so that changing a property on either atom will
+			// propagate to the other.  If a map is provided for `keyOrListOrMap`,
+			// properties on this atom may be bound to differently named properties
+			// on `otherAtom`.
+			entangle: function (otherAtom, keyOrListOrMap) {
+				var
+					isList = isArray(keyOrListOrMap),
+					isMap = !isList && isObject(keyOrListOrMap),
+					i, key,
+					keys = isList ? keyOrListOrMap : isMap ? [] : [keyOrListOrMap],
+					map = isMap ? keyOrListOrMap : {}
+				;
+				if (isMap) {
+					for (key in map) {
+						if (map.hasOwnProperty(key)) {
+							keys.push(key);
+						}
+					}
+				} else {
+					for (i = keys.length; --i >= 0;) {
+						key = keys[i];
+						map[key] = key;
+					}
+				}
+				me.each(keys, function (key) {
+					var otherKey = map[key];
+					me.bind(key, function (value) {
+						otherAtom.set(otherKey, value);
+					});
+					otherAtom.bind(otherKey, function (value) {
+						me.set(key, value);
+					});
+				});
+			},
+
+			// Get current values for the specified keys.  If `func` is provided,
+			// it will be called with the values as args.
+			get: function (keyOrList, func) {
+				return get(nucleus, keyOrList, func);
+			},
+
+			// Returns true iff all of the specified keys exist (regardless of
+			// value).
+			has: function (keyOrList) {
+				var keys = toArray(keyOrList);
+				for (var i = keys.length; --i >= 0;) {
+					if (!props.hasOwnProperty(keys[i])) {
+						return false;
+					}
+				}
+				return true;
+			},
+
+			// Return a list of all keys.
+			keys: function () {
+				var keys = [];
+				for (var key in props) {
+					if (props.hasOwnProperty(key)) {
+						keys.push(key);
+					}
+				}
+				return keys;
+			},
+
+			// Add arbitrary properties to this atom's interface.
+			mixin: function (obj) {
+				for (var p in obj) {
+					if (obj.hasOwnProperty(p)) {
+						me[p] = obj[p];
+					}
+				}
+				return me;
+			},
+
+			// Call `func` as soon as all of the specified keys have been set.  If
+			// they are already set, the function will be called immediately, with
+			// all the values provided as args.  In this, it is identical to
+			// `once()`.  However, calling `need()` will additionally invoke
+			// providers when possible, in order to try and create the required
+			// values.
+			need: function (keyOrList, func) {
+				var key, keys = toArray(keyOrList), values, provider;
+				for (var i = keys.length; --i >= 0;) {
+					key = keys[i];
+					provider = providers[key];
+					if (!props.hasOwnProperty(key) && provider) {
+						provide(nucleus, key, provider);
+						delete providers[key];
+					} else {
+						needs[key] = true;
+					}
+				}
+				me.once(keys, func);
+			},
+
+			// Call `func` whenever any of the specified keys is next changed.  The
+			// values of all keys will be provided as args to the function.  The
+			// function will automatically be unbound after being called the first
+			// time, so it is guaranteed to be called no more than once.
+			next: function (keyOrList, func) {
+				listeners.unshift(
+					{ keys: toArray(keyOrList), cb: func, all: false, calls: 1 });
+			},
+
+			// Call `func` as soon as all of the specified keys have been set.  If
+			// they are already set, the function will be called immediately, with
+			// all the values provided as args.  Guaranteed to be called no more
+			// than once.
+			once: function (keyOrList, func) {
+				var keys = toArray(keyOrList),
+					values = get(nucleus, keys, null, true);
+				if (values) {
+					func.apply({}, values);
+				} else {
+					listeners.unshift(
+						{ keys: keys, cb: func, all: true, calls: 1 });
+				}
+			},
+
+			// Register a provider for a particular key.  The provider `func` is a
+			// function that will be called if there is a need to create the key.
+			// It must call its first arg as a callback, with the value.  Provider
+			// functions will be called at most once.
+			provide: function (key, func) {
+				if (needs[key]) {
+					provide(nucleus, key, func);
+				} else if (!providers[key]) {
+					providers[key] = func;
+				}
+			},
+
+			// Set value for a key, or if `keyOrMap` is an object then set all the
+			// keys' corresponding values.
+			set: function (keyOrMap, value) {
+				if (isObject(keyOrMap)) {
+					for (var key in keyOrMap) {
+						if (keyOrMap.hasOwnProperty(key)) {
+							set(nucleus, key, keyOrMap[key]);
+						}
+					}
+				} else {
+					set(nucleus, keyOrMap, value);
+				}
+			},
+
+			// Unregister a listener `func` that was previously registered using
+			// `bind()`, `need()`, `next()` or `once()`.
+			unbind: function (func) {
+				for (var i = listeners.length; --i >= 0;) {
+					if (listeners[i].cb === func) {
+						listeners.splice(i, 1);
+					}
+				}
+			}
+		};
+		return me;
+	};
+
+}());
